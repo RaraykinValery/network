d1229877e41df4058a2e4cc095bbe55d314ec9ac
Valery Raraykin <vand_er@mail.ru>
2022-01-13 09:18:17 +0300

    20 navLink

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
     return (
         <div>
             <div>
-                <img src="https://wallpaperboat.com/wp-content/uploads/2020/07/30/51519/sea-02.jpg" alt="" />
+                <img width='800' src="https://wallpaperboat.com/wp-content/uploads/2020/07/30/51519/sea-02.jpg" alt="" />
             </div>
             <div>ava + descr</div>
             <MyPosts />
